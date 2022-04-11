46539998f9f246a95352f381cf10065e5ded48c0
germolinal <germolinal@gmail.com>
2022-04-11 11:35:25 +0000

    Deploying to gh-pages from @ SIMPLE-BuildingSimulation/rendering@37ff06e85886859aba8c7fcdf187954929afeeca 🚀

---
+++ rendering/material/ward/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"fn":[["evaluate_ward_anisotropic","Evaluates a Ward BSDF, changing the direction of a given Ray. Returns a tuple with the  Specular and Diffuse reflections"],["sample_ward_anisotropic","Samples a Ward BSDF, changing the direction of a given Ray. Returns a tuple with the  Specular and Diffuse reflections, as well as the Weighting factor for the BSDF"]]});
