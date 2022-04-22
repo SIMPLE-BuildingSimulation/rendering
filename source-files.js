28f78b670a38dee54bb2060e52c99087a2ed007b
germolinal <germolinal@gmail.com>
2022-04-22 11:45:08 +0000

    Deploying to gh-pages from @ SIMPLE-BuildingSimulation/rendering@1a30bb7bd4ac6076015d0594bb1a7e30df02ce0d 🚀

--- source-files.js
+++ source-files.js
@@ -1,4 +1,4 @@
 var N = null;var sourcesIndex = {};
 sourcesIndex["rendering"] = {"name":"","dirs":[{"name":"camera","files":["camera_trait.rs","film.rs","mod.rs","pinhole.rs","sample.rs","view.rs"]},{"name":"material","files":["dielectric.rs","glass.rs","light.rs","metal.rs","mirror.rs","mod.rs","plastic.rs","specular.rs","ward.rs"]}],"files":["bvh.rs","colour.rs","colourmap.rs","from_obj.rs","from_radiance.rs","from_simple_model.rs","image.rs","interaction.rs","lib.rs","primitive.rs","primitive_samplers.rs","rand.rs","ray.rs","ray_tracer.rs","samplers.rs","scene.rs","triangle.rs"]};
-sourcesIndex["spict"] = {"name":"","files":["bin.rs"]};
+sourcesIndex["spict"] = {"name":"","files":["spict.rs"]};
 createSourceSidebar();
